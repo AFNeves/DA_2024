13bc57bf1d7302f06d5397e3d32b40e7c047b3fe
Afonso Neves <up202108884@up.pt>
2024-03-01 20:40:31 +0000

    initial Pipe implementation

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -1,1 +1,25 @@
 #include "Pipe.h"
+
+/* Constructor */
+
+Pipe::Pipe(Node *a, Node *b, int capacity, bool biDirection) : A(a), B(b), capacity(capacity), biDirection(biDirection) {}
+
+/* Getters */
+
+Node *Pipe::getA() const { return A; }
+
+Node *Pipe::getB() const { return B; }
+
+int Pipe::getCapacity() const { return capacity; }
+
+bool Pipe::isBiDirectional() const { return biDirection; }
+
+/* Setters */
+
+void Pipe::setA(Node *newNode) { A = newNode; }
+
+void Pipe::setB(Node *newNode) { B = newNode; }
+
+void Pipe::setCapacity(int newCapacity) { Pipe::capacity = newCapacity; }
+
+void Pipe::setBiDirection(bool newDirection) { Pipe::biDirection = newDirection; }
