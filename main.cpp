08e8aeb37c5dd84d79db5d47cbd7b5d752e1cc9e
Afonso Neves <up202108884@up.pt>
2024-03-08 15:47:31 +0000

    updated main to test parsing

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
-
-#include "src/Station.h"
-#include "src/Pipe.h"
-
-using namespace std;
+#include "Network.h"
 
 int main()
 {
-    Station station1(3,"S-09");
-    Station station2(6,"S-52");
-
-    Pipe pipe(&station1, &station2, 50);
-
-    cout << "\nStation | ID:" << pipe.getSrc()->getID() << " Code:" << pipe.getSrc()->getCode() << "\n";
-    cout << "\nStation | ID:" << pipe.getDest()->getID() << " Code:" << pipe.getDest()->getCode() << "\n";
-    cout << "\nPipe | Capacity:" << pipe.getCapacity() << "\n\n";
-
+    Network network;
     return 0;
 }
