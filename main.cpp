552f58267fb50eb2501f09e3677d7c04392f4b9c
Afonso Neves <up202108884@up.pt>
2024-03-29 17:45:46 +0000

    Max Flow test2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include "Network.h"
 
+#include "Node.h"
+
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
     Network network;
+
+    Node *s = network.findNode("R_1");
+    Node *t = network.findNode("C_10");
+
+    int maxFlow = network.edmondsKarp(s,t);
+
+    cout << "\nMax Flow between " << s->getCode() << " and " << t->getCode() << ": " << maxFlow << endl;
+
     return 0;
 }
