3a42094d87d0869eb0a4aa5f70f483cd3bb2e8d4
Valter Castro <valterochoacastro@gmail.com>
2024-04-05 22:39:15 +0100

    Moodle Tests

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Network.h"
+#include "Reservoir.h"
 
 #include "Node.h"
 
@@ -11,11 +12,36 @@ int main()
     Network network;
 
     Node *s = network.findNode("S_SRC");
-    Node *t = network.findNode("C_1");
+    vector <Node *> cities;
+    vector <int> maxFlows;
+    int expected_total_flow = 0;
 
-    int maxFlow = network.edmondsKarp(s,t);
+    for(int i = 1 ; i < 11; ++i){
 
-    cout << "\nMax Flow between " << s->getCode() << " and " << t->getCode() << ": " << maxFlow << endl;
+        Node *t = network.findNode("C_" + to_string(i));
+        cities.push_back(t);
+
+        int maxFlow = network.edmondsKarp(s, t);
+        maxFlows.push_back(maxFlow);
+        expected_total_flow += maxFlow;
+
+        cout << "\nMax Flow between " << s->getCode() << " and " << t->getCode() << ": " << maxFlow << endl;
+    }
+
+    cout << "\nExpected_total_flow: " << expected_total_flow << endl;
+
+    int funchalDemand = cities[5]->getCapacity();
+    int funchalActualFlow = maxFlows[5];
+    cout << "\nC_6-Funchal" << "\nDemand: " << cities[5]->getCapacity() << "\nActual Flow: " << funchalActualFlow << "\nDeficit: " << funchalDemand - funchalActualFlow << endl;
+
+
+    network.resetNetwork();
+
+    Node *r = network.findNode("R_4");
+    delete r;
+
+    cout << network.edmondsKarp(s, cities[5]) << endl;
+
 
     return 0;
 }
