d6f2272d7318d6d88ebbfbfa6fc3a0c4c80524f2
Valter Castro <valterochoacastro@gmail.com>
2024-04-07 17:18:05 +0100

    Tests done

--- main.cpp
+++ main.cpp
@@ -7,15 +7,7 @@
 
 using namespace std;
 
-int main()
-{
-    Network network;
-
-    Node *s = network.findNode("S_SRC");
-    vector <Node *> cities;
-    vector <int> maxFlows;
-    int expected_total_flow = 0;
-
+void citiesFlows(vector<int> &maxFlows, vector<Node *> &cities, Network &network , Node *s, int &expected_total_flow){
     for(int i = 1 ; i < 11; ++i){
 
         Node *t = network.findNode("C_" + to_string(i));
@@ -27,6 +19,18 @@ int main()
 
         cout << "\nMax Flow between " << s->getCode() << " and " << t->getCode() << ": " << maxFlow << endl;
     }
+}
+
+int main()
+{
+    Network network;
+
+    Node *s = network.findNode("S_SRC");
+    vector <Node *> cities;
+    vector <int> maxFlows;
+    int expected_total_flow = 0;
+
+    citiesFlows(maxFlows, cities, network, s, expected_total_flow);
 
     cout << "\nExpected_total_flow: " << expected_total_flow << endl;
 
@@ -37,11 +41,81 @@ int main()
 
     network.resetNetwork();
 
-    Node *r = network.findNode("R_4");
-    delete r;
+    Node *ribeiro_frio = network.findNode("R_4");
+    delete ribeiro_frio;
 
-    cout << network.edmondsKarp(s, cities[5]) << endl;
+    cout << "Machico: " << "Old Flow: " << maxFlows[3] << " New Flow: " << network.edmondsKarp(s, cities[3]) << endl;
+    cout << "Santa Cruz: " << "Old Flow: " << maxFlows[4] << " New Flow: " << network.edmondsKarp(s, cities[4]) << endl;
+    cout << "Funchal: " << "Old Flow: " << maxFlows[5] << " New Flow: " << network.edmondsKarp(s, cities[5]) << endl;
+
+    network.resetNetwork();
+    cout << endl;
+
+    Network network1;
+
+    cout << endl;
+
+    Node *ps1 = network1.findNode("PS_1");
+    Node *s1 = network1.findNode("S_SRC");
+    delete ps1;
+
+    cout << "Santa Cruz: " << "Old Flow: " << maxFlows[4] << " New Flow: " << network1.edmondsKarp(s1, cities[4]) << endl;
+    cout << "Funchal: " << "Old Flow: " << maxFlows[5] << " New Flow: " << network1.edmondsKarp(s1, cities[5]) << endl;
+    cout << "Porto Moniz: " << "Old Flow: " << maxFlows[0] << " New Flow: " << network1.edmondsKarp(s1, cities[0]) << endl;
+    cout << "Calheta: " << "Old Flow: " << maxFlows[9] << " New Flow: " << network1.edmondsKarp(s1, cities[9]) << endl;
 
 
+    cout << "\nCase 1:\n";
+
+    Network network2;
+
+    Node *s2 = network2.findNode("S_SRC");
+    Node *ps9 = network2.findNode("PS_9");
+    Node *ps10 = network2.findNode("PS_10");
+    delete ps9;
+    delete ps10;
+    maxFlows.clear();
+    cities.clear();
+    expected_total_flow = 0;
+
+    citiesFlows(maxFlows, cities, network2, s2, expected_total_flow);
+
+    cout << "\nCase 2:\n";
+
+    Network network3;
+
+    Node *s3 = network3.findNode("S_SRC");
+    Node *ps4 = network3.findNode("PS_4");
+    Node *ps5 = network3.findNode("PS_5");
+    delete ps4;
+    delete ps5;
+    maxFlows.clear();
+    cities.clear();
+    expected_total_flow = 0;
+
+    citiesFlows(maxFlows, cities, network, s3, expected_total_flow);
+
+
+    cout << "\nCase 3:\n";
+
+    Network network4;
+
+    Node *s4 = network4.findNode("S_SRC");
+    Node *ps4_ = network4.findNode("PS_4");
+    Node *ps5_ = network4.findNode("PS_5");
+    Node *ps9_ = network4.findNode("PS_9");
+    Node *ps10_ = network4.findNode("PS_10");
+
+    delete ps9_;
+    delete ps10_;
+    delete ps4_;
+    delete ps5_;
+
+
+    maxFlows.clear();
+    cities.clear();
+    expected_total_flow = 0;
+
+    citiesFlows(maxFlows, cities, network, s4, expected_total_flow);
     return 0;
 }
